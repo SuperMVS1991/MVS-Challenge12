f15fb425a78f506794034b87e95a64ab23761d5c
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-02-19 11:32:51 -0500

    added quary.sql, schema.sql, seeds.sql, package.json, and server.js that i created to github

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (department_name)
+VALUES ('Sales');
+("IT");
+("Finance");
+("Legal");
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1);
+("Salesperson", 80000, 1);
+("Lead Engineer", 150000, 2);
+("Software Engineer", 120000, 2);
+("Accountant", 125000, 3);
+("Legal Team Lead", 250000, 4);
+("Lawyer", 190000, 4);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('John', 'Doe', 1, 1);
+("Mike", "Chan", 2, 2);
+("Ashley", "Rodriguez", 3, 3);
+("Kevin", "Tupik", 4, 4);
+("Kunal", "Singh", 5, 5);
+("Malia", "Brown", 6, 6);
+("Sarah", "Lourd", 7, 7);
+("Tom", "Allen", 8, 8);
+("Tina", "Lee", 9, 9);
+("Mark", "Taylor", 10, 10);
+("Katie", "Smith", 11, 11);
+
